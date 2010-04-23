95a4503765e705bee121c7c653c59981eaf76d02
Thomas Ploch <t.ploch@reizwerk.com>
2010-04-23 10:39:25 +0200

    Updated initialize migration with utf-8 encoding

--- config/migrations/map.php
+++ config/migrations/map.php
@@ -1,6 +1,6 @@
 <?php
 $map = array(
 	1 => array(
-		'001_initialize_userplugin' => 'M4bd157e3ad7c4272ad5f0e4266eab0dd'),
+		'001_initialize_userplugin_schema' => 'M4bd15c6a82a84a00bda70fa766eab0dd'),
 );
 ?>
